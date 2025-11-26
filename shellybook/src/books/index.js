9faabf779c3b5d1082f4b14ced2b954575c39a3f
Елена <ira.fad33va@mail.com>
2025-11-27 04:13:32 +0700

    Добавление конфигурационного файла для импорта книг

---
+++ shellybook/src/books/index.js
@@ -0,0 +1,5 @@
+// Этот файл поможет импортировать тексты книг
+export { default as shinelText } from './gogol_shinel.txt';
+export { default as damaText } from './chekhov_dama.txt';
+export { default as prestuplenieText } from './dostoevsky_prestuplenie.txt';
+export { default as kavkazskyText } from './tolstoy_kavkazsky_plennik.txt';
